82a53f458cfa1958eb753f33259d95a9d6deb7c5
sanka047 <niranjan.msankar@gmail.com>
2022-01-13 10:57:18 -0600

    feat: disable netrw

--- nvim/lua/sanka047/core/plugins.lua
+++ nvim/lua/sanka047/core/plugins.lua
@@ -1,4 +1,16 @@
 --------------------------------------------------------------------------------
 -- Load Plugins
 --------------------------------------------------------------------------------
+-- disable unused builtins
+local builtins = {
+    'netrw',
+    'netrwPlugin',
+    'netrwSettings',
+    'netrwFileHandlers',
+}
+
+for _, plugin in pairs(builtins) do
+    vim.g['loaded_' .. plugin] = 1
+end
+
 require('sanka047.plugins.init')
