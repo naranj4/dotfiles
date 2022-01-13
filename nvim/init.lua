b0a05adbbc4bbeb06594b07d83d5c8190f9da8c4
sanka047 <niranjan.msankar@gmail.com>
2022-01-13 00:57:49 -0600

    chore: remove old nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,12 @@
 --------------------------------------------------------------------------------
--- Import Lua Modules
+-- Neovim Config File
 --------------------------------------------------------------------------------
-require('settings')
-require('plug')
-require('plug-config')
-require('keymaps')
-require('colorscheme')
+-- Design:
+-- - Plugin mapping will be available prior to the plugin has initialized
+-- - Plugins will load their configs when they are loaded by packer
+-- - Packer will be downloaded on first load
+require('sanka047.core.globals')
+require('sanka047.core.options')
+require('sanka047.core.plugins')
+require('sanka047.core.colors')
+require('sanka047.core.mappings')
