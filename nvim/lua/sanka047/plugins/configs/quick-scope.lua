6869291a9aef78c6869b0ef7c40e9fba44eeffbe
sanka047 <niranjan.msankar@gmail.com>
2022-01-13 23:43:40 -0600

    feat: keep quickscope always turned on

--- nvim/lua/sanka047/plugins/configs/quick-scope.lua
+++ nvim/lua/sanka047/plugins/configs/quick-scope.lua
@@ -1,9 +1,8 @@
 --------------------------------------------------------------------------------
 -- Quickscope Config
 --------------------------------------------------------------------------------
--- TODO: quickscope broken on start up
 vim.g.qs_enable = 1
-vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+-- vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
 vim.g.qs_lazy_highlight = 1
 vim.g.qs_delay = 100
 vim.g.qs_hi_priority = 2
