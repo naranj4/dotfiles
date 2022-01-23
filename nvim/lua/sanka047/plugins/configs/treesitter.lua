636b168a335c18803c7d2bdf273a86c1478904f6
sanka047 <niranjan.msankar@gmail.com>
2022-01-23 12:54:42 -0600

    fix: move playground config to appropriate place

--- nvim/lua/sanka047/plugins/configs/treesitter.lua
+++ nvim/lua/sanka047/plugins/configs/treesitter.lua
@@ -73,29 +73,31 @@ treesitter_configs.setup({
                 ['[]'] = '@class.outer',
             },
         },
-        playground = {
-            enable = true,
-            disable = {},
-            updatetime = 50,
-            persist_queries = false,
-            keybindings = {
-                toggle_query_editor = 'o',
-                toggle_hl_groups = 'i',
-                toggle_injected_languages = 't',
-                toggle_anonymous_nodes = 'a',
-                toggle_language_display = 'I',
-                focus_language = 'f',
-                unfocus_language = 'F',
-                update = 'R',
-                goto_node = '<cr>',
-                show_help = '?',
-            }
-        },
-        query_linter = {
-            enable = true,
-            use_virtual_text = true,
-            lint_events = { 'BufWrite', 'CursorHold' },
-        },
+    },
+
+    playground = {
+        enable = true,
+        disable = {},
+        updatetime = 50,
+        persist_queries = false,
+        keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+        }
+    },
+
+    query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { 'BufWrite', 'CursorHold' },
     },
 })
 treesitter_install.prefer_git = true
