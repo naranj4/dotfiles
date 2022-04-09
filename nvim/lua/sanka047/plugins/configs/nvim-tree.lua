ffd638c704efb224509c24bb6049e8247347f2b8
sanka047 <niranjan.msankar@gmail.com>
2022-04-09 14:16:52 -0500

    fix: update nvim-tree configuration

--- nvim/lua/sanka047/plugins/configs/nvim-tree.lua
+++ nvim/lua/sanka047/plugins/configs/nvim-tree.lua
@@ -8,29 +8,18 @@ if not ok then
 end
 
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_quit_on_open = 1  -- 0 by default, closes the tree when you open a file
 vim.g.nvim_tree_indent_markers = 1  -- 0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 1  -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1  -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = ':~'  -- This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_add_trailing = 1  -- 0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 1  --  0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_disable_window_picker = 0  -- 0 by default, will disable the window picker.
 vim.g.nvim_tree_icon_padding = ' '  -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
 vim.g.nvim_tree_symlink_arrow = ' ➛ '  --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
 vim.g.nvim_tree_respect_buf_cwd = 1  -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 vim.g.nvim_tree_create_in_closed_folder = 0  -- 1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 vim.g.nvim_tree_refresh_wait = 500  -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-vim.g.nvim_tree_window_picker_exclude = {
-    filetype = {
-        'notify',
-        'packer',
-        'qf'
-    },
-    buftype = {
-        'terminal'
-    },
-}
+
 -- Dictionary of buffer option names mapped to a list of option values that
 -- indicates to the window picker that the buffer's window should not be
 -- selectable.
@@ -82,7 +71,6 @@ nvim_tree.setup({
     hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
-    auto_close = true,
     open_on_tab = false,
     hijack_cursor = true,
     update_cwd = true,
@@ -131,8 +119,29 @@ nvim_tree.setup({
         relativenumber = false,
         signcolumn = "yes"
     },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+            window_picker = {
+                enable = 1,
+                exclude = {
+                    filetype = {
+                        'notify',
+                        'packer',
+                        'qf'
+                    },
+                    buftype = {
+                        'terminal'
+                    },
+                },
+            },
+        },
+    },
     trash = {
         cmd = "trash",
         require_confirm = true
     }
 })
+
+-- Auto-closing
+vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
