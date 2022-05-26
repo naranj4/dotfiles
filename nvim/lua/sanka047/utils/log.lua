82391feb51112d52b1ae5d80e07fb17985efd550
sanka047 <niranjan.msankar@gmail.com>
2022-05-26 00:56:14 -0500

    refactor: expose log as a generic function

--- nvim/lua/sanka047/utils/log.lua
+++ nvim/lua/sanka047/utils/log.lua
@@ -3,26 +3,24 @@
 --------------------------------------------------------------------------------
 local M = {}
 
-function M.log(msg, hl, name)
-    name = name or "Neovim"
-    hl = hl or "Todo"
-    vim.api.nvim_echo({ { name .. ": ", hl }, { msg } }, true, {})
+function M.log(log_level, msg, title)
+    vim.notify(msg, log_level, { title = title })
 end
 
-function M.debug(msg, name)
-    vim.notify(msg, vim.log.levels.DEBUG, { title = name })
+function M.debug(msg, title)
+    M.log(vim.log.levels.DEBUG, msg, title)
 end
 
-function M.info(msg, name)
-    vim.notify(msg, vim.log.levels.INFO, { title = name })
+function M.info(msg, title)
+    M.log(vim.log.levels.INFO, msg, title)
 end
 
-function M.warn(msg, name)
-    vim.notify(msg, vim.log.levels.WARN, { title = name })
+function M.warn(msg, title)
+    M.log(vim.log.levels.WARN, msg, title)
 end
 
-function M.error(msg, name)
-    vim.notify(msg, vim.log.levels.ERROR, { title = name })
+function M.error(msg, title)
+    M.log(vim.log.levels.ERROR, msg, title)
 end
 
 return M
