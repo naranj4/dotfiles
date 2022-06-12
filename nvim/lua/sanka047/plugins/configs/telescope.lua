decf438c861c2eb89af0ce2af900c450e63a7cc6
sanka047 <niranjan.msankar@gmail.com>
2022-06-12 01:19:12 -0500

    fix(telescope): ensure that height/width are integers

--- nvim/lua/sanka047/plugins/configs/telescope.lua
+++ nvim/lua/sanka047/plugins/configs/telescope.lua
@@ -84,14 +84,14 @@ function M.setup()
                     prompt_position = 'bottom',
                     height = 0.9,
                     width = function (self, max_columns, max_lines)
-                        local width = max_columns * 0.8
+                        local width = math.ceil(max_columns * 0.8)
                         if width > 120 then
                             width = 120
                         end
                         return width
                     end,
                     preview_height = function (self, max_columns, max_lines)
-                        local height = max_lines * 0.3
+                        local height = math.ceil(max_lines * 0.3)
                         if height < 5 then
                             height = 5
                         elseif height > 15 then
