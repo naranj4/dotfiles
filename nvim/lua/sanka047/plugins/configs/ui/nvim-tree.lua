e201a1d1f8192f32fd1609113c1345f90f759abf
sanka047 <niranjan.msankar@gmail.com>
2022-06-25 18:48:54 -0500

    chore(nvim-tree): update config

--- nvim/lua/sanka047/plugins/configs/ui/nvim-tree.lua
+++ nvim/lua/sanka047/plugins/configs/ui/nvim-tree.lua
@@ -15,95 +15,28 @@ function M.setup()
     end
 
     nvim_tree.setup({
-        disable_netrw = true,
-        hijack_netrw = true,
-        open_on_setup = false,
-        ignore_ft_on_setup = {},
-        open_on_tab = false,
+        hijack_netrw = false,
         hijack_cursor = true,
         update_cwd = true,
-        update_to_buf_dir = {
-            enable = true,
-            auto_open = true,
-        },
-        diagnostics = {
-            enable = true,
-            icons = {
-                hint = "",
-                info = "",
-                warning = "",
-                error = "",
-            }
-        },
-        update_focused_file = {
-            enable = false,
-            update_cwd = false,
-            ignore_list = {}
-        },
-        system_open = {
-            cmd = nil,
-            args = {}
-        },
-        filters = {
-            dotfiles = false,
-            custom = {}
-        },
-        git = {
-            enable = true,
-            ignore = true,
-            timeout = 500,
-        },
+        diagnostics = { enable = true },
         view = {
             width = 50,
-            height = 30,
-            hide_root_folder = false,
-            side = 'left',
-            auto_resize = false,
             mappings = {
-                custom_only = false,
-                list = {
-                    { key = '?', action = 'toggle_help' },
-                },
+                list = { { key = '?', action = 'toggle_help' } },
             },
-            number = false,
-            relativenumber = false,
-            signcolumn = 'yes',
         },
         renderer = {
-            indent_markers = {
-                enable = true,
-            },
+            indent_markers = { enable = true },
             highlight_git = true,
             highlight_opened_files = 'name',
             group_empty = true,
             add_trailing = true,
         },
+        hijack_directories = { enable = false },
         actions = {
-            open_file = {
-                quit_on_open = true,
-                window_picker = {
-                    enable = true,
-                    exclude = {
-                        filetype = {
-                            'notify',
-                            'packer',
-                            'qf'
-                        },
-                        buftype = {
-                            'terminal'
-                        },
-                    },
-                },
-            },
+            open_file = { quit_on_open = true },
         },
-        trash = {
-            cmd = "trash",
-            require_confirm = true
-        }
     })
-
-    -- Auto-closing
-    vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
 end
 
 --------------------------------------------------------------------------------
