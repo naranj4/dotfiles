d3ec335af1a7df6b0c1fe1d2130b5b74b72b94c8
sanka047 <niranjan.msankar@gmail.com>
2022-07-09 17:30:55 -0500

    feat(lsp.config): add diagnostic signs

--- nvim/lua/sanka047/plugins/lsp/config.lua
+++ nvim/lua/sanka047/plugins/lsp/config.lua
@@ -92,6 +92,13 @@ function M.setup()
         { border = window.border(window.margin.FULL) }
     )
 
+    -- setup signs/highlights
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
     configure_lsp_servers()
 end
 
