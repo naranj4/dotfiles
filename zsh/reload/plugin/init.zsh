e07ab5c4d6c3e9127b33900b9c4281b69c683051
sanka047 <niranjan.msankar@gmail.com>
2022-07-09 14:48:33 -0500

    feat(zsh): fzf plugin
    Closes #9

--- zsh/reload/plugin/init.zsh
+++ zsh/reload/plugin/init.zsh
@@ -16,13 +16,7 @@ zplug "romkatv/powerlevel10k", as:theme, depth:1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions"
 
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
+zplug "junegunn/fzf", use:"shell/*.zsh"
 
 zplug load
 
