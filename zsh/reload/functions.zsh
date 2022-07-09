1803710465800736f0596bd16b7f7837204b6240
sanka047 <niranjan.msankar@gmail.com>
2022-07-09 15:35:32 -0500

    feat(zsh): replace cat with bat if available
    Closes #8

--- zsh/reload/functions.zsh
+++ zsh/reload/functions.zsh
@@ -6,3 +6,14 @@
 function greload {
     source ~/.config/zsh/reload/init.zsh
 }
+
+# Simple bat output with fallback to cat
+function simple_bat {
+    if ! [ -x "$(command -v bat)" ]; then
+        cat $argv
+    else
+        bat --style=header-filename,header-filesize,snip,grid $argv
+    fi
+}
+
+alias cat="simple_bat"
