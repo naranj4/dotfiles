274dcc3bab8943b98bde8155c09da620bd174c6e
sanka047 <niranjan.msankar@gmail.com>
2022-08-14 01:50:58 -0500

    refactor(emacs): use general.el for mappings feat(emacs): add projectile

--- emacs/init.el
+++ emacs/init.el
@@ -25,13 +25,54 @@
         (eval-print-last-sexp)))
       (load bootstrap-file nil 'nomessage))
 
-;; Install use-package for rest of configuration
+;; Install use-package/general for rest of configuration
 (straight-use-package 'use-package)
+
+                                        ; Package Management
+(use-package straight
+  :custom (straight-use-package-by-default t))
+
+(use-package no-littering
+  :init
+  (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
+        backup-directory-alist `((".*" . ,(no-littering-expand-var-file-name "back-up/")))
+        lock-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "lock-files/") t))
+        custom-file (locate-user-emacs-file "custom-vars.el"))
+  (load custom-file 'noerror 'nomessage))
+
+                                        ; Vi-Emulation
+(use-package goto-chg)
+
+(use-package evil
+  :custom
+  (evil-undo-system 'undo-redo)
+  (evil-want-C-u-scroll t)
+  (evil-want-Y-yank-to-eol t)
+  (evil-disable-insert-state-bindings t)
+  :init
+  (setq evil-insert-state-cursor 'bar
+        evil-motion-state-cursor 'hbar
+        evil-operator-state-cursor 'hbar)
+  :config
+  (evil-mode 1)
+
+  (evil-set-leader '(normal visual operator motion) (kbd "SPC")))
+
+                                        ; Mapping Enhancements
+(use-package general
+  :config
+  (general-define-key :states 'motion
+    "s-<left>" 'back-to-indentation
+    "H" 'back-to-indentation
+    "s-<right>" 'evil-end-of-line
+    "L" 'evil-end-of-line
+    "Q" 'evil-window-delete))
 
                                         ; Emacs Configuration
 (use-package emacs
   :straight nil
   :no-require
+  :defer nil
   :preface
   ;; Delete word instead of kill word
   (defun my/delete-word (arg)
@@ -67,29 +108,18 @@ This command does not push text to `kill-ring'."
   ;; default to disabling using tabs for indents
   (setq-default indent-tabs-mode nil)
 
-  :config
+  :general
   ;; keybinds for clipboard copy/paste
-  (global-set-key (kbd "s-v") 'clipboard-yank)
-  (global-set-key (kbd "s-c") 'clipboard-kill-ring-save)
+  ("s-v" 'clipboard-yank)
+  ("s-c" 'clipboard-kill-ring-save)
 
   ;; remap deletion keybinds
-  (global-set-key (kbd "<M-backspace>") 'my/backward-delete-word)
-  (global-set-key (kbd "M-d") 'my/delete-word)
+  ("<M-backspace>" 'my/backward-delete-word)
+  ("M-d" 'my/delete-word)
 
+  :config
   ;; refresh buffers if files were changed on disk (unless unsaved)
   (global-auto-revert-mode 1))
-
-                                        ; Package Management
-(use-package straight
-  :custom (straight-use-package-by-default t))
-
-(use-package no-littering
-  :init
-  (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
-        backup-directory-alist `((".*" . ,(no-littering-expand-var-file-name "back-up/")))
-        lock-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "lock-files/") t))
-        custom-file (locate-user-emacs-file "custom-vars.el"))
-  (load custom-file 'noerror 'nomessage))
 
                                         ; History
 (use-package savehist
@@ -175,17 +205,16 @@ This command does not push text to `kill-ring'."
 
                                         ; UI Enhancements
 (use-package helpful
-  :bind
-  ([remap describe-function] . helpful-callable)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . helpful-variable)
-  ([remap describe-key] . helpful-key)
+  :general
+  ("C-h f" 'helpful-callable)
+  ("C-h x" 'helpful-command)
+  ("C-h v" 'helpful-variable)
+  ("C-h k" 'helpful-key)
+  ("C-h F" 'helpful-function)
+  ("C-c C-d" 'helpful-at-point)
 
-  ("C-h F" . helpful-function)
-  ("C-c C-d" . helpful-at-point)
-  :config
-  (with-eval-after-load 'evil-maps
-    (evil-define-key 'normal helpful-mode-map (kbd "q") 'quit-window)))
+  (:keymaps 'helpful-mode-map :states 'normal
+            "q" 'quit-window))
 
 (use-package which-key
   :custom
@@ -197,42 +226,13 @@ This command does not push text to `kill-ring'."
   :straight (popon :type git :repo "https://codeberg.org/akib/emacs-popon")
   :if (not (display-graphic-p)))
 
-                                        ; Vi-Emulation
-(use-package goto-chg)
-
-(use-package evil
-  :custom
-  (evil-undo-system 'undo-redo)
-  (evil-want-C-u-scroll t)
-  (evil-want-Y-yank-to-eol t)
-  (evil-disable-insert-state-bindings t)
-  :init
-  (setq evil-insert-state-cursor 'bar
-        evil-motion-state-cursor 'hbar
-        evil-operator-state-cursor 'hbar)
-  :config
-  (evil-mode 1)
-
-  (evil-set-leader '(normal visual operator motion) (kbd "SPC"))
-
-  (evil-global-set-key 'motion (kbd "H") 'back-to-indentation)
-  (evil-global-set-key 'motion (kbd "L") 'evil-end-of-line)
-
-  (evil-global-set-key 'normal (kbd "Q") 'evil-window-delete))
-
-(use-package better-jumper
-  :config
-  (better-jumper-mode 1)
-  (with-eval-after-load 'evil-maps
-    (evil-global-set-key 'motion (kbd "C-o") 'better-jumper-jump-backward)
-    (evil-global-set-key 'motion (kbd "C-i") 'better-jumper-jump-forward)))
-
                                         ; Terminal Replacement
 (use-package vterm
   :custom (vterm-always-compile-module t)
-  :config
-  (with-eval-after-load 'evil-maps
-    (evil-define-key 'insert 'vterm-mode-map (kbd "C-u") 'vterm-send-C-u)))
+  :general
+  (:keymaps 'vterm-mode-map :states 'insert
+            "C-u" 'vterm-send-C-u
+            "C-d" 'vterm-send-C-d))
 
                                         ; Editing Enhancements
 (use-package evil-surround
@@ -242,10 +242,9 @@ This command does not push text to `kill-ring'."
                 evil-surround-pairs-alist (push '(?> . ("<" . ">")) evil-surround-pairs-alist)))
 
 (use-package evil-nerd-commenter
-  :config
-  (with-eval-after-load 'evil-maps
-    (evil-global-set-key 'normal (kbd "<leader>c") 'evilnc-comment-operator)
-    (evil-global-set-key 'visual (kbd "<leader>c") 'evilnc-comment-or-uncomment-lines)))
+  :general
+  (:states 'normal "<leader>c" 'evilnc-comment-operator)
+  (:states 'visual "<leader>c" 'evilnc-comment-or-uncomment-lines))
 
 ;; autopairs
 (use-package electric-pair
@@ -253,7 +252,15 @@ This command does not push text to `kill-ring'."
   :no-require
   :config (electric-pair-mode 1))
 
-                                        ; Movement Enhancements
+                                        ; Navigation Enhancements
+(use-package better-jumper
+  :general
+  (:states 'motion
+           "C-o" 'better-jumper-jump-backward
+           "C-i" 'better-jumper-jump-forward)
+  :config
+  (better-jumper-mode 1))
+
 (use-package evil-visualstar
   :config (global-evil-visualstar-mode 1))
 
@@ -263,33 +270,41 @@ This command does not push text to `kill-ring'."
   (avy-keys '(?s ?l ?a ?g ?h ?v ?m ?e ?i ?r ?u ?w ?o ?c ?x ?d ?k ?f ?j))
   (avy-orders-alist '((avy-goto-char-2 . avy-order-closest)
                       (avy-goto-line . avy-order-closest)))
+  :general
+  (:states 'motion
+           "<leader>j" 'evil-avy-goto-line-below
+           "<leader>k" 'evil-avy-goto-line-above)
+  (:states '(normal visual operator motion) "s" 'evil-avy-goto-char-2))
+
+(use-package projectile
+  :general
+  ;; TODO: remap this to just the list of commands I use
+  (:states 'normal
+           "<leader>f" 'projectile-command-map)
   :config
-  (with-eval-after-load 'evil-maps
-    (evil-define-key '(normal visual operator motion) 'global (kbd "s") 'evil-avy-goto-char-2)
-    (evil-global-set-key 'motion (kbd "<leader>j") 'evil-avy-goto-line-below)
-    (evil-global-set-key 'motion (kbd "<leader>k") 'evil-avy-goto-line-above)))
+  (projectile-mode 1))
 
                                         ; Window Management
 (use-package windmove
   :custom (windmove-create-window t)
   :commands (windmove-left windmove-down windmove-up windmove-right)
-  :config
-  (with-eval-after-load 'evil-maps
-    (evil-global-set-key 'normal (kbd "S-<left>") 'windmove-left)
-    (evil-global-set-key 'normal (kbd "S-<down>") 'windmove-down)
-    (evil-global-set-key 'normal (kbd "S-<up>") 'windmove-up)
-    (evil-global-set-key 'normal (kbd "S-<right>") 'windmove-right)))
+  :general
+  (:states 'normal
+           "S-<left>" 'windmove-left
+           "S-<down>" 'windmove-down
+           "S-<up>" 'windmove-up
+           "S-<right>" 'windmove-right))
 
                                         ; Version Control
 (use-package diff-hl
+  :general
+  (:states 'motion
+           "]c" 'diff-hl-next-hunk
+           "[c" 'diff-hl-previous-hunk
+           "<leader>hs" 'diff-hl-stage-current-hunk
+           "<leader>hU" 'diff-hl-unstage-file)
   :config
-  (global-diff-hl-mode 1)
-
-  (with-eval-after-load 'evil-maps
-    (evil-global-set-key 'motion (kbd "]c") 'diff-hl-next-hunk)
-    (evil-global-set-key 'motion (kbd "[c") 'diff-hl-previous-hunk)
-    (evil-global-set-key 'normal (kbd "<leader>hs") 'diff-hl-stage-current-hunk)
-    (evil-global-set-key 'normal (kbd "<leader>hU") 'diff-hl-unstage-file)))
+  (global-diff-hl-mode 1))
 
                                         ; Completion Enhancements
 (use-package vertico
@@ -306,6 +321,8 @@ This command does not push text to `kill-ring'."
 
   :custom
   (vertico-cycle t)
+  (vertico-count 20)
+
   ;; hide commands in M-x which do not work in the current mode.
   (read-extended-command-predicate #'command-completion-default-include-p)
   (enable-recursive-minibuffers t)
@@ -315,6 +332,15 @@ This command does not push text to `kill-ring'."
   (vertico-mode 1))
 
 (use-package orderless
+  :preface
+  (defun my/match-components-literally ()
+    "Components match literally for the rest of this minibuffer session."
+    (interactive)
+    (setq-local orderless-matching-styles 'orderless-literal
+                orderless-style-dispatchers nil))
+
+  (general-define-key :keymaps 'vertico-map "C-l" 'my/match-components-literally)
+
   :custom
   (orderless-matching-styles '(orderless-literal orderless-regexp orderless-prefixes orderless-flex))
   (completion-styles '(orderless basic))
@@ -327,6 +353,8 @@ This command does not push text to `kill-ring'."
 (use-package consult)
 
 (use-package corfu
+  :defer nil
+
   :preface
   (defun my/corfu-enable-in-minibuffer ()
     "Enable Corfu in the minibuffer if Vertico/Mct are not active."
@@ -335,18 +363,22 @@ This command does not push text to `kill-ring'."
       (corfu-mode 1)))
   (add-hook 'minibuffer-setup-hook #'my/corfu-enable-in-minibuffer 1)
 
+  ;; Quit Corfu after exiting insert mode
+  :hook (evil-insert-state-exit . corfu-quit)
+
   :custom
   (corfu-cycle t)
   (corfu-auto t)
   (corfu-auto-delay 0)
   (corfu-auto-prefix 2)
 
-  :config
-  (global-corfu-mode 1)
+  :general
+  (:keymaps 'corfu-map
+            "RET" nil
+            "M-S-SPC" 'corfu-insert-separator)
 
-  (with-eval-after-load 'evil-maps
-    (evil-define-key 'insert 'corfu-map (kbd "RET") 'newline)
-    (evil-define-key 'insert 'corfu-map (kbd "M-S-SPC") 'corfu-insert-separator)))
+  :config
+  (global-corfu-mode 1))
 
 (use-package corfu-terminal
   :straight (corfu-terminal :type git :repo "https://codeberg.org/akib/emacs-corfu-terminal")
@@ -357,11 +389,11 @@ This command does not push text to `kill-ring'."
 (use-package corfu-doc
   :requires corfu
   :hook (corfu-mode . corfu-doc-mode)
-  :config
-  (with-eval-after-load 'evil-maps
-    (evil-define-key 'insert 'corfu-map (kbd "M-d") #'corfu-doc-toggle)
-    (evil-define-key 'insert 'corfu-map (kbd "M-p") #'corfu-doc-scroll-up)
-    (evil-define-key 'insert 'corfu-map (kbd "M-n") #'corfu-doc-scroll-down)))
+  :general
+  (:keymaps 'corfu-map
+            "M-d" 'corfu-doc-toggle
+            "M-p" 'corfu-doc-scroll-down
+            "M-n" 'corfu-doc-scroll-up))
 
 (use-package corfu-doc-terminal
   :straight (corfu-doc-terminal :type git :repo "https://codeberg.org/akib/emacs-corfu-doc-terminal")
