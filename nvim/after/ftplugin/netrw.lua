78ab2c4fd6f0441c31e98d66c955b8d15d228212
sanka047 <niranjan.msankar@gmail.com>
2022-09-19 00:27:30 -0500

    chore(netrw): close netrw and return to original buffer using Q

--- nvim/after/ftplugin/netrw.lua
+++ nvim/after/ftplugin/netrw.lua
@@ -3,6 +3,9 @@ local map = function (...) require('sanka047.utils.map').buf_map(0, ...) end
 local mappings = require('sanka047.core.mappings')
 local bidirectional_leap = require('sanka047.plugins.nav.leap').bidirectional_leap
 
+-- return to original buffer
+map('n', 'Q', 'quit-netrw', '<CMD>Rexplore<CR>')
+
 -- window switching mappings to override defaults
 mappings.win_move({ buffer = 0 })
 
