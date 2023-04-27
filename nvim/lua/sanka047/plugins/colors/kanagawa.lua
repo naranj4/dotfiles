8cd998e2b0fd4d93808130bd29ba93325a243138
Niranjan Sankar <nisankar@amazon.com>
2023-04-27 18:11:03 +0000

    fix: breaking changes to colorscheme

--- nvim/lua/sanka047/plugins/colors/kanagawa.lua
+++ nvim/lua/sanka047/plugins/colors/kanagawa.lua
@@ -15,103 +15,109 @@ function M.setup()
     vim.g.kanagawa_lualine_bold = true
 
     -- Overrides
-    local colors = require('kanagawa.colors').setup()
-    local overrides = {
-        InclineNormal = { bg = colors.fn, fg = colors.bg_dark, bold = true },
-        InclineNormalNC = { bg = colors.bg_light0, fg = colors.fg_dark },
+    local overrides = function (colors)
+        local palette = colors.palette
+        local theme = colors.theme
+        return {
+            InclineNormal = { bg = theme.syn.fun, fg = theme.ui.float.bg, bold = true },
+            InclineNormalNC = { bg = theme.ui.bg_light0, fg = theme.ui.float.fg },
 
-        HopNextKey = { fg = colors.fg, bold = true },
-        HopNextKey1 = { link = 'HopNextKey' },
-        HopNextKey2 = { fg = colors.fg },
-        HopUnmatched = { bg = colors.bg, fg = colors.fg_comment },
-        HopCursor = { link = 'Cursor' },
+            HopNextKey = { fg = theme.ui.fg, bold = true },
+            HopNextKey1 = { link = 'HopNextKey' },
+            HopNextKey2 = { fg = theme.ui.fg },
+            HopUnmatched = { bg = theme.ui.bg, fg = theme.syn.comment },
+            HopCursor = { link = 'Cursor' },
 
-        FloatBorder = { fg = colors.bg_dark, bg = colors.bg },
-        FloatTitle = { fg = colors.bg_dark, bg = colors.waveAqua2, bold = true },
+            FloatBorder = { fg = theme.ui.float.bg, bg = theme.ui.bg },
+            FloatTitle = { fg = theme.ui.float.bg, bg = palette.waveAqua2, bold = true },
 
-        DressingNormal = { fg = colors.fg, bg = colors.winterBlue },
-        DressingBorder = { fg = colors.winterBlue, bg = colors.bg },
+            DressingNormal = { fg = theme.ui.fg, bg = palette.winterBlue },
+            DressingBorder = { fg = palette.winterBlue, bg = theme.ui.bg },
 
-        CmpCompletionBorder = { fg = colors.bg_menu, bg = colors.bg },
-        CmpDocumentation = { bg = colors.bg_dim },
-        CmpDocumentationBorder = { fg = colors.bg_dim, bg = colors.bg },
+            CmpCompletionBorder = { fg = theme.ui.pmenu.bg, bg = theme.ui.bg },
+            CmpDocumentation = { bg = theme.ui.bg_dim },
+            CmpDocumentationBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg },
 
-        QuickScopePrimary = { fg = colors.crystalBlue, underline = true, bold = true },
-        QuickScopeSecondary = { fg = colors.diag.warning, underline = true, bold = true },
+            QuickScopePrimary = { fg = palette.crystalBlue, underline = true, bold = true },
+            QuickScopeSecondary = { fg = theme.diag.warning, underline = true, bold = true },
 
-        LeapMatch = { link = 'QuickScopeSecondary' },
-        LeapLabelPrimary = { fg = colors.bg_dark, bg = colors.carpYellow, bold = true },
-        LeapLabelSecondary = { fg = colors.bg_dark, bg = colors.crystalBlue, bold = true },
-        LeapBackdrop = { fg = colors.fg_comment, bg = colors.bg },
+            LeapMatch = { link = 'QuickScopeSecondary' },
+            LeapLabelPrimary = { fg = theme.ui.float.bg, bg = palette.carpYellow, bold = true },
+            LeapLabelSecondary = { fg = theme.ui.float.bg, bg = palette.crystalBlue, bold = true },
+            LeapBackdrop = { fg = theme.syn.comment, bg = theme.ui.bg },
 
-        ToggletermNormal = { fg = colors.fg, bg = colors.bg },
-        ToggletermBorder = { fg = colors.fg_border, bg = colors.bg },
+            ToggletermNormal = { fg = theme.ui.fg, bg = theme.ui.bg },
+            ToggletermBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg },
 
-        TelescopeNormal = { fg = colors.fg_dark, bg = colors.winterBlue },
-        TelescopeBorder = { fg = colors.winterBlue, bg = colors.winterBlue },
-        TelescopeTitle = { fg = colors.winterBlue, bg = colors.crystalBlue, bold = true },
+            TelescopePromptTitle = { fg = theme.ui.fg_reverse, bg = palette.autumnYellow, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p2 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p2, bg = theme.ui.bg_p2 },
 
-        TelescopePromptNormal = { fg = colors.fg_dark, bg = colors.bg_menu },
-        TelescopePromptBorder = { fg = colors.bg_menu, bg = colors.bg_menu },
-        TelescopePromptTitle = { fg = colors.bg, bg = colors.autumnYellow, bold = true },
+            TelescopeResultsTitle = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_p1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
 
-        TelescopeResultsTitle = { fg = colors.winterBlue, bg = colors.winterBlue, bold = true },
+            TelescopePreviewTitle = { fg = theme.ui.fg_reverse, bg = palette.boatYellow2, bold = true },
+            TelescopePreviewNormal = { bg = theme.ui.bg_p1 },
+            TelescopePreviewBorder = { bg = theme.ui.bg_p1, fg = theme.ui.bg_p1 },
 
-        TelescopePreviewTitle = { fg = colors.bg, bg = colors.boatYellow2, bold = true },
+            CmpItemAbbrDeprecated = { fg = palette.katanaGray, bg = "NONE", strikethrough = true },
+            CmpItemAbbrMatch = { fg = palette.crystalBlue, bg = "NONE", bold = true },
+            CmpItemAbbrMatchFuzzy = { fg = palette.crystalBlue, bg = "NONE", bold = true },
 
-        CmpItemAbbrDeprecated = { fg = colors.katanaGray, bg = "NONE", strikethrough = true },
-        CmpItemAbbrMatch = { fg = colors.crystalBlue, bg = "NONE", bold = true },
-        CmpItemAbbrMatchFuzzy = { fg = colors.crystalBlue, bg = "NONE", bold = true },
+            CmpItemKindField = { fg = theme.ui.pmenu.bg, bg = palette.autumnGreen },
+            CmpItemKindProperty = { fg = theme.ui.pmenu.bg, bg = palette.autumnGreen },
+            CmpItemKindEvent = { fg = theme.ui.pmenu.bg, bg = palette.autumnGreen },
 
-        CmpItemKindField = { fg = colors.bg_menu, bg = colors.autumnGreen },
-        CmpItemKindProperty = { fg = colors.bg_menu, bg = colors.autumnGreen },
-        CmpItemKindEvent = { fg = colors.bg_menu, bg = colors.autumnGreen },
+            CmpItemKindText = { fg = theme.ui.pmenu.bg, bg = palette.springGreen },
+            CmpItemKindEnum = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
+            CmpItemKindKeyword = { fg = theme.ui.pmenu.bg, bg = palette.oniViolet },
 
-        CmpItemKindText = { fg = colors.bg_menu, bg = colors.springGreen },
-        CmpItemKindEnum = { fg = colors.bg_menu, bg = colors.carpYellow },
-        CmpItemKindKeyword = { fg = colors.bg_menu, bg = colors.oniViolet },
+            CmpItemKindConstant = { fg = theme.ui.pmenu.bg, bg = palette.surimiOrange },
+            CmpItemKindConstructor = { fg = theme.ui.pmenu.bg, bg = palette.surimiOrange },
+            CmpItemKindReference = { fg = theme.ui.pmenu.bg, bg = palette.surimiOrange },
 
-        CmpItemKindConstant = { fg = colors.bg_menu, bg = colors.surimiOrange },
-        CmpItemKindConstructor = { fg = colors.bg_menu, bg = colors.surimiOrange },
-        CmpItemKindReference = { fg = colors.bg_menu, bg = colors.surimiOrange },
+            CmpItemKindFunction = { fg = theme.ui.pmenu.bg, bg = palette.crystalBlue },
+            CmpItemKindStruct = { fg = theme.ui.pmenu.bg, bg = palette.waveAqua2 },
+            CmpItemKindClass = { fg = theme.ui.pmenu.bg, bg = palette.waveAqua2 },
 
-        CmpItemKindFunction = { fg = colors.bg_menu, bg = colors.crystalBlue },
-        CmpItemKindStruct = { fg = colors.bg_menu, bg = colors.waveAqua2 },
-        CmpItemKindClass = { fg = colors.bg_menu, bg = colors.waveAqua2 },
+            CmpItemKindModule = { fg = theme.ui.pmenu.bg, bg = palette.surimiOrange },
+            CmpItemKindOperator = { fg = palette.carpYellow, bg = palette.boatYellow2 },
 
-        CmpItemKindModule = { fg = colors.bg_menu, bg = colors.surimiOrange },
-        CmpItemKindOperator = { fg = colors.carpYellow, bg = colors.boatYellow2 },
+            CmpItemKindVariable = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
+            CmpItemKindFile = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
 
-        CmpItemKindVariable = { fg = colors.bg_menu, bg = colors.carpYellow },
-        CmpItemKindFile = { fg = colors.bg_menu, bg = colors.carpYellow },
+            CmpItemKindUnit = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
+            CmpItemKindSnippet = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
+            CmpItemKindFolder = { fg = theme.ui.pmenu.bg, bg = palette.carpYellow },
 
-        CmpItemKindUnit = { fg = colors.bg_menu, bg = colors.carpYellow },
-        CmpItemKindSnippet = { fg = colors.bg_menu, bg = colors.carpYellow },
-        CmpItemKindFolder = { fg = colors.bg_menu, bg = colors.carpYellow },
+            CmpItemKindMethod = { fg = theme.ui.pmenu.bg, bg = palette.crystalBlue },
+            CmpItemKindValue = { fg = theme.ui.pmenu.bg, bg = palette.crystalBlue },
+            CmpItemKindEnumMember = { fg = theme.ui.pmenu.bg, bg = palette.crystalBlue },
 
-        CmpItemKindMethod = { fg = colors.bg_menu, bg = colors.crystalBlue },
-        CmpItemKindValue = { fg = colors.bg_menu, bg = colors.crystalBlue },
-        CmpItemKindEnumMember = { fg = colors.bg_menu, bg = colors.crystalBlue },
-
-        CmpItemKindInterface = { fg = colors.bg_menu, bg = colors.sakuraPink },
-        CmpItemKindColor = { fg = colors.bg_menu, bg = colors.sakuraPink },
-        CmpItemKindTypeParameter = { fg = colors.bg_menu, bg = colors.sakuraPink },
-    }
+            CmpItemKindInterface = { fg = theme.ui.pmenu.bg, bg = palette.sakuraPink },
+            CmpItemKindColor = { fg = theme.ui.pmenu.bg, bg = palette.sakuraPink },
+            CmpItemKindTypeParameter = { fg = theme.ui.pmenu.bg, bg = palette.sakuraPink },
+        }
+    end
 
     kanagawa.setup({
-        undercurl = true,
         commentStyle = { italic = true },
         functionStyle = { bold = true },
         keywordStyle = { bold = true },
         statementStyle = { bold = true },
         typeStyle = { bold = true },
         variablebuiltinStyle = { bold = true },
-        specialReturn = true,
-        specialException = true,
         transparent = false,
         dimInactive = false, -- while shade is broken
         globalStatus = true,
-        colors = {},
+        colors = {
+            theme = {
+                all = {
+                    ui = { bg_gutter = "none" },
+                },
+            },
+        },
         overrides = overrides,
     })
 
